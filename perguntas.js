f3e21822eb7b14f0d0f5fe78bf513d8155f02cfb
giuli1310 <162493826+giuli1310@users.noreply.github.com>
2024-11-29 22:37:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Geografia',
+    'Qual a xapital da Itália?',
+    'Roma'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 36',
+    '6'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu estou bem em Inglês?',
+    'Oi em ingles é i am fine'
+)
